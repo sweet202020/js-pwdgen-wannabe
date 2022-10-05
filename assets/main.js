0e908a73a0ab79ed9f5c52bb0f1ab3a211a9c03e
sweet202020 <dolcealessio1@gmail.com>
2022-10-05 16:23:00 +0200

    fix stamp id

--- assets/main.js
+++ assets/main.js
@@ -1,14 +1,16 @@
 const MyName = prompt("qual è il tuo nome?")
 console.log(MyName);
-document.getElementById("name").innerHTML=(MyName)
+
 const surname = prompt("qual è il tuo cognome")
 console.log(surname);
-document.getElementById("surname").innerHTML= (surname)
+
 const favouriteColor = prompt("qual è il tuo colore preferito?")
 console.log(favouriteColor);
-document.getElementById("favourite_color").innerHTML= (favourite_color)
+
 let currentYear = (22)
 console.log(currentYear);
-document.getElementById("year").innerHTML= (currentYear)
+
+document.getElementById("stamp").innerHTML= (MyName+ " " +surname + " " +favouriteColor+ " " +currentYear)
+
 
 
